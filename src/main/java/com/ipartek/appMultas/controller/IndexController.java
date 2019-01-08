d87b98aa1c6f8e8c61e0c3414a32c0ae10a75655
dZallo <danielzallo@gmail.com>
2019-01-08 14:20:29 +0100

    Busca el agente registrado y lo mete en la session

--- src/main/java/com/ipartek/appMultas/controller/IndexController.java
+++ src/main/java/com/ipartek/appMultas/controller/IndexController.java
@@ -1,11 +1,17 @@
 package com.ipartek.appMultas.controller;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ipartek.appMultas.modelo.dao.AgenteDAO;
+import com.ipartek.appMultas.modelo.pojo.Agente;
 
 /**
  * Servlet implementation class IndexController
@@ -14,12 +20,32 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/index")
 public class IndexController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private AgenteDAO dao;
+	
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		dao =AgenteDAO.getInstance();
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		dao=null;
+	}
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//Cargar usuario
+		Long idAgente= 4L;
+		
+		Agente agente = dao.getByID(idAgente);
+		
+		HttpSession session= request.getSession();
+		session.setAttribute("agenteLogueado", agente);
+		session.setMaxInactiveInterval(6*100);
 		
 		//Redireccionar a index.jsp
 		request.getRequestDispatcher("index.jsp").forward(request, response);
