10df098ae18ef7dc8182b9ce8e94b600f076dd59
AnderSolana <andersolanad@gmail.com>
2019-01-16 12:52:20 +0100

    Creación de página .jsp para realizar la acción de login con su respectivo controller para el control de acceso

---
+++ src/main/java/com/ipartek/appMultas/controller/LoginController.java
@@ -0,0 +1,67 @@
+package com.ipartek.appMultas.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ipartek.appMultas.modelo.dao.AgenteDAO;
+import com.ipartek.appMultas.modelo.pojo.Agente;
+import com.ipartek.appMultas.modelo.pojo.Mensaje;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private AgenteDAO dao;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		dao = AgenteDAO.getInstance();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String placa = request.getParameter("placa");
+		String password = request.getParameter("password");
+		Agente agente = dao.login(placa, password);
+		
+		if (placa.equals("") || password.equals("")) {
+			request.setAttribute("placa", placa);
+			request.setAttribute("password", password);
+			request.setAttribute("mensaje", new Mensaje(Mensaje.TIPO_DANGER, "No puede haber campos vacíos"));
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+		}else {
+		if (agente == null ) {
+			request.setAttribute("mensaje", new Mensaje(Mensaje.TIPO_DANGER, "Nº Placa o Contraseña no válidos"));
+			request.setAttribute("placa", placa);
+			request.setAttribute("password", password);
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+		}else {
+			HttpSession session = request.getSession();
+			session.setAttribute("agenteLogueado", agente);
+			session.setAttribute("mensaje", null);
+			// Redireccionar a index.jsp
+			request.getRequestDispatcher("privado/index.jsp").forward(request, response);
+		}
+		}
+	}
+
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,21 @@
+<%@include file="includes/cabecera.jsp" %>
+<body class="text-center">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+<main role="main" class="inner cover">
+    <h1 class="cover-heading"><i class="material-icons" style="font-size:114px;">directions_car</i></h1>
+    <p class="lead">Introduzca sus credenciales: </p>
+    <form action="login" method="POST">
+    <div class="form-group">	    
+	    <label for="placa">Nº Placa: </label>
+	    <input type="number" class="form-control" id="placa" name="placa" 
+	    	   aria-describedby="placaHelp" placeholder="P.E: 1234" value="${placa}" required>
+  	</div>
+  	<div class="form-group">	    
+	    <label for="password">Contraseña: </label>
+	    <input type="password" class="form-control" id="password" name="password" 
+	    	   aria-describedby="passwordHelp" placeholder="P.E: 12345678" value="${password}" required>
+  	</div>
+  	<button type="submit" class="btn btn-lg btn-secondary btn-block">Acceder</button>
+  	</form>
+<%@include file="includes/mensajes.jsp" %>
+<%@include file="includes/pie.jsp" %>
