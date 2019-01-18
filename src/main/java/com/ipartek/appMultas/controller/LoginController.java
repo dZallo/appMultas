a6386fa34052e6afde2c04e0c87c14be55b12d67
AnderSolana <andersolanad@gmail.com>
2019-01-18 10:44:34 +0100

    Limpieza de el controlador LoginController (Quitados imports no necesarios, código formateado y simplificadas acciones)

--- src/main/java/com/ipartek/appMultas/controller/LoginController.java
+++ src/main/java/com/ipartek/appMultas/controller/LoginController.java
@@ -1,7 +1,6 @@
 package com.ipartek.appMultas.controller;
 
 import java.io.IOException;
-import java.util.Calendar;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -14,7 +13,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 
 import com.ipartek.appMultas.modelo.dao.AgenteDAO;
-import com.ipartek.appMultas.modelo.dao.MultaDAO;
 import com.ipartek.appMultas.modelo.pojo.Agente;
 import com.ipartek.appMultas.modelo.pojo.Mensaje;
 
@@ -25,86 +23,63 @@ import com.ipartek.appMultas.modelo.pojo.Mensaje;
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private AgenteDAO daoAgente;
-	private MultaDAO daoMulta;
-	
-	private Agente agenteSession = null;
+
 	private HttpSession session = null;
-	
+
 	private final static Logger LOG = Logger.getLogger(LoginController.class);
-	
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		daoAgente = AgenteDAO.getInstance();
-		daoMulta = MultaDAO.getInstance();
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String placa = request.getParameter("placa");
 		String password = request.getParameter("password");
 		Agente agente = daoAgente.login(placa, password);
-		
+
 		if (placa.equals("") || password.equals("")) {
 			request.setAttribute("placa", placa);
 			request.setAttribute("password", password);
 			request.setAttribute("mensaje", new Mensaje(Mensaje.TIPO_DANGER, "No puede haber campos vacíos"));
 			request.getRequestDispatcher("login.jsp").forward(request, response);
-		}else {
-		if (agente == null ) {
-			request.setAttribute("mensaje", new Mensaje(Mensaje.TIPO_DANGER, "Nº Placa o Contraseña no válidos"));
-			request.setAttribute("placa", placa);
-			request.setAttribute("password", password);
-			request.getRequestDispatcher("login.jsp").forward(request, response);
-		}else {
-			agenteSession = agente;
-			session = getSession(request);
-			//session.setAttribute("agenteLogueado", agenteSession);
+		} else {
+			if (agente == null) {
+				request.setAttribute("mensaje", new Mensaje(Mensaje.TIPO_DANGER, "Nº Placa o Contraseña no válidos"));
+				request.setAttribute("placa", placa);
+				request.setAttribute("password", password);
+				request.getRequestDispatcher("login.jsp").forward(request, response);
+			} else {
+				session = getSession(request);
+				LOG.debug("Agente Logueado: " + agente.toString());
+				session.setAttribute("agenteLogueado", daoAgente.obtenerImportes(agente));
+				session.setAttribute("mensaje", null);
+				// Redireccionar a index.jsp
+				response.sendRedirect("privado/index.jsp");
+			}
+		}
+	}
 
-			//agenteSession = getAgenteSession(session);
-			session.setAttribute("agenteLogueado", daoAgente.obtenerImportes(agenteSession));
-			session.setAttribute("mensaje", null);
-			// Redireccionar a index.jsp
-			//request.getRequestDispatcher("privado/index.jsp").forward(request, response);
-			response.sendRedirect("privado/index.jsp");
-		}
-		}
-	}
-	
-	private Agente getAgenteSession( HttpSession session) {
-		agenteSession= (Agente) session.getAttribute("agenteLogueado");
-		
-		return agenteSession;
-	}
-	
-	private HttpSession getSession( HttpServletRequest request) {
+	private HttpSession getSession(HttpServletRequest request) {
 		session = request.getSession();
-		
+
 		return session;
 	}
-	
-//	private int obtenerAño() {
-//		Calendar cal = Calendar.getInstance();
-//		int year = cal.get(Calendar.YEAR);
-//		LOG.debug("Año actual para la búsqueda de estadísticas: " + year);
-//		return year;
-//	}
-//	
-//	private int obtenerMes() {
-//		Calendar cal = Calendar.getInstance();
-//		int month = cal.get(Calendar.MONTH);
-//		LOG.debug("Mes actual para la búsqueda de estadísticas: " + month + 1);
-//		return month + 1;
-//	}
 
 }
